82ff116f9ec2a249812ae112c2e9375073bc5e8b
monilecode <monilebox@gmail.com>
2024-11-04 22:17:01 +0200

    get and post methods

--- node-nd/src/server.js
+++ node-nd/src/server.js
@@ -1,11 +1,69 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const categories = [
+  {
+    id: 1,
+    category: "cleaning",
+    icon: "/src/components/categories/images/cleaning.png",
+    alt: "cleaning",
+  },
+  {
+    id: 2,
+    category: "repair",
+    icon: "/src/components/categories/images/repair.png",
+    alt: "repair",
+  },
+  {
+    id: 3,
+    category: "painting",
+    icon: "/src/components/categories/images/painting.png",
+    alt: "painting",
+  },
+  {
+    id: 4,
+    category: "shifting",
+    icon: "/src/components/categories/images/shifting.png",
+    alt: "shifting",
+  },
+  {
+    id: 5,
+    category: "plumbing",
+    icon: "/src/components/categories/images/plumbing.png",
+    alt: "plumbing",
+  },
+  {
+    id: 6,
+    category: "electric",
+    icon: "/src/components/categories/images/electric.png",
+    alt: "electric",
+  },
+];
 
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(morgan("dev"));
 
-app.get("/");
+app.get("/api/categories", (req, res) => {
+  res.json(categories);
+});
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.post("/api/categories", (req, res) => {
+  const newId = categories.length + 1;
+  categories.push({ id: newId, name: req.body.name });
+
+  res.json({
+    success: true,
+    message: "Category added successfully",
+    categoryId: newId,
+  });
+});
+
+app.listen(process.env.API_PORT, () => {
+  console.log(
+    `Server is running on ${process.env.API_PROTOCOL}://${process.env.API_HOST}:${process.env.API_PORT}`
+  );
 });
